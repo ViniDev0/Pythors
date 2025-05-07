1167cf3f8266aee335fd75b7a9ae08e6b5275df5
ViniDev0 <vogomesoliveira@gmail.com>
2025-05-07 15:56:45 -0300

    Solved activity 01 and 02 for the exam

---
+++ activity01.py
@@ -0,0 +1,8 @@
+inputValue = float(input("Digite um valor:"));
+
+if (inputValue > 100):
+  inputValue -= 10;
+  print("Seu valor e superior a 100 pontos.");
+print(f"Valor: {inputValue}");
+
+
---
+++ activity02.py
@@ -0,0 +1,7 @@
+inputValue01 = int(input("Digite o primeiro valor:"));
+inputValue02 = int(input("Digite o segundo valor:"));
+inputValue03 = int(input("Digite o terceiro valor:"));
+
+Biggest = max(inputValue01, inputValue02, inputValue03);
+
+print(f"Maior Valor: {Biggest}")
